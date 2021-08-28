0faa505f16868aa3b97618c86340110349eb8cdd
NayuTS <26164205+tasotasoso@users.noreply.github.com>
2021-08-28 11:11:43 +0900

    q2_3を修正する

--- chap2/q2_3/main.go
+++ chap2/q2_3/main.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"popcount/popcount"
 	"strconv"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println(strconv.Itoa(popcount.PopCount(100)))
+	end := time.Now()
+	fmt.Printf("最初のバージョンは%f秒\n", (end.Sub(start)).Seconds())
+
+	start = time.Now()
 	fmt.Println(strconv.Itoa(popcount.RoopPopCount(100)))
+	end = time.Now()
+	fmt.Printf("修正後のバージョンは%f秒\n", (end.Sub(start)).Seconds())
 }
